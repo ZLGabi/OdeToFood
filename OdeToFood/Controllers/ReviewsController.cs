1e199d09ceb091c63821bcce5ffe2708bf26225d
zbantl <zbantl@RO-ZBANTL>
2018-07-02 17:43:44 +0300

    HTML Helpers done

--- OdeToFood/Controllers/ReviewsController.cs
+++ OdeToFood/Controllers/ReviewsController.cs
@@ -49,22 +49,22 @@ namespace OdeToFood.Controllers
         // GET: Reviews/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            var review = _reviews.Single(r => r.Id == id);
+            return View(review);
         }
 
         // POST: Reviews/Edit/5
         [HttpPost]
         public ActionResult Edit(int id, FormCollection collection)
         {
-            try
+            var review = _reviews.Single(r => r.Id == id);
+            if(TryUpdateModel(review))
             {
-                // TODO: Add update logic here
-
                 return RedirectToAction("Index");
             }
-            catch
+            else
             {
-                return View();
+                return View(review);
             }
         }
 
